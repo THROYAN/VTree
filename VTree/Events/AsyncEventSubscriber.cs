8029e4147339b522fe01c6e4ca5d65d1ac4d4cbb
THROYAN <throyan@gmail.com>
2017-03-03 04:02:20 +0200

    + AsyncEventSubscriber to run event handlers in different threads

---
+++ VTree/Events/AsyncEventSubscriber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VTree.Events
+{
+    public class AsyncEventSubscriber<T>
+        where T : EventArgs
+    {
+        private Queue<T> query = new Queue<T>();
+
+        public event EventHandler<T> OnEvent;
+
+        public EventHandler<T> Handler
+        {
+            get
+            {
+                return (object sender, T e) =>
+                {
+                    lock (this.query)
+                    {
+                        this.query.Enqueue(e);
+                        Monitor.Pulse(this.query);
+                    }
+                };
+            }
+        }
+
+        public Thread Thread
+        {
+            get
+            {
+                return new Thread(() =>
+                {
+                    while (true)
+                    {
+                        lock (this.query)
+                        {
+                            while (this.query.Count == 0)
+                            {
+                                Monitor.Wait(this.query);
+                            }
+                            T e = this.query.Dequeue();
+
+                            // this??
+                            this.OnEvent?.Invoke(this, e);
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
